8e51ba5951650f0b18978a68d94e715081902624
dawson <dawson09@163.com>
2024-10-04 21:58:05 +0800

    Initialize database

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TripApi.Models;
+
+namespace TripApi.Database;
+
+/// <summary>
+/// Database context configuration
+/// </summary>
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TouristRoute> TouristRoutes { get; set; }
+
+    public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TripApi.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("NgSql");
+
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
