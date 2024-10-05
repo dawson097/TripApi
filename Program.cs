224347a99c92fe38319cd895679ea5e5142baa4a
dawson <dawson09@163.com>
2024-10-05 12:20:43 +0800

    Add data repository and registry repository service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using TripApi.Database;
+using TripApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
---
+++ Services/ITouristRouteRepository.cs
@@ -0,0 +1,22 @@
+﻿using TripApi.Models;
+
+namespace TripApi.Services;
+
+/// <summary>
+/// Repository service of tourist route
+/// </summary>
+public interface ITouristRouteRepository
+{
+    /// <summary>
+    /// Get all of tourist route
+    /// </summary>
+    /// <returns></returns>
+    IEnumerable<TouristRoute> GetAllRoutes();
+
+    /// <summary>
+    /// Get single tourist route with route id
+    /// </summary>
+    /// <param name="routeId"></param>
+    /// <returns></returns>
+    TouristRoute GetRouteById(Guid routeId);
+}
