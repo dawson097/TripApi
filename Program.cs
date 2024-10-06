9e1af1881f9482c633f4a0607d8e8330997e522f
dawson <dawson09@163.com>
2024-10-06 14:23:47 +0800

    Output status code 422

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TripApi.Database;
 using TripApi.Services;
@@ -8,7 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-}).AddXmlDataContractSerializerFormatters();
+}).AddXmlDataContractSerializerFormatters()
+.ConfigureApiBehaviorOptions(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var problemDetail = new ValidationProblemDetails(context.ModelState)
+        {
+            Type = "Valid Error",
+            Title = "数据验证失败",
+            Status = StatusCodes.Status422UnprocessableEntity,
+            Detail = "请看详细说明",
+            Instance = context.HttpContext.Request.Path
+        };
+
+        problemDetail.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+        return new UnprocessableEntityObjectResult(problemDetail)
+        {
+            ContentTypes = { "application/problem+json" }
+        };
+    };
+});
 
 builder.Services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
 builder.Services.AddTransient<ITouristRoutePictureRepository, TouristRoutePictureRepository>();
