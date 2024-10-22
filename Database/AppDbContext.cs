766989dd43608d8abe4f3fa9af58020df97b8b1e
dawson <dawson09@163.com>
2024-10-23 00:18:26 +0800

    Add data seeding

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using TripApi.Models;
 
 namespace TripApi.Database;
@@ -12,4 +14,17 @@ public class AppDbContext : DbContext
     public DbSet<TouristRoute> TouristRoutes { get; set; }
 
     public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var routesData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+                                          "/Database/tourist-routes.json");
+        var routes = JsonConvert.DeserializeObject<IList<TouristRoute>>(routesData);
+        modelBuilder.Entity<TouristRoute>().HasData(routes);
+
+        var routePicturesData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+                                                 "/Database/tourist-route-pictures.json");
+        var routePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(routePicturesData);
+        modelBuilder.Entity<TouristRoutePicture>().HasData(routePictures);
+    }
 }
--- Migrations/20241022161754_Add data seeding.Designer.cs
+++ Migrations/20241022161754_Add data seeding.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TripApi.Database;
@@ -11,9 +12,11 @@ using TripApi.Database;
 namespace TripApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241022161754_Add data seeding")]
+    partial class Adddataseeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
